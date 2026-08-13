463fa7ebdaf49df38ad1bd019c31ba459277df70
Palle Martin <177297762+PalleMartin@users.noreply.github.com>
2026-08-13 10:20:46 +0530

    Add files via upload

--- Android Studios/build.gradle.kts
+++ Android Studios/build.gradle.kts
@@ -1,4 +1,41 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.application)
+}
+
+android {
+    namespace = "com.routecj.myapplication"
+    compileSdk = 37
+
+    defaultConfig {
+        applicationId = "com.routecj.myapplication"
+        minSdk = 26
+        targetSdk = 37
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            optimization {
+                enable = false
+            }
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.activity.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(libs.androidx.junit)
 }
